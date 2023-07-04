2ccda322158363c890acfcce2c23954fc2b7536f
FacuCasado <facufcasado@gmail.com>
2023-07-04 10:44:01 -0300

    Force por Alter

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,7 @@ const server=require('./src/app')
 const {sequelize}=require('./src/db');
 const PORT=process.env.SERVER_PORT || 3001
 
-sequelize.sync({force:true}).then(()=>{
+sequelize.sync({alter:true}).then(()=>{
     server.listen(PORT,()=>{
         console.log(`server listenign at ${PORT}`);
     })
